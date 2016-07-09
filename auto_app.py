8ffacea8369b9509af5c6aff6bb44093537a672d
oiertwo <oier.etxaniz@gmail.com>
2016-07-09 14:34:21 +0200

    deleted ep2015 media

--- auto_app.py
+++ auto_app.py
@@ -1,6 +1,7 @@
 import subprocess
 
 commands = [
+    "fuser -k 5000/tcp", #kill process in port 5000
     "pip install virtualenv",
     "virtualenv env -p /usr/bin/python2.7",
     "env/bin/pip install -r requirements/dev_requirements.txt",
